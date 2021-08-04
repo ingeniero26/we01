f2825f69b5cba34130e6715b2cdae8fdbfc153dc
ingeniero26 <ingjerson2014@gmail.com>
2021-08-04 15:34:54 -0500

    proyecto personal

---
+++ index.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portafolio</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css"> 
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="css/animate.min.css">
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+
+    <section id="nav-bar">
+        <nav class="navbar navbar-expand-lg navbar-light ">
+          <a class="navbar-brand" href="#"><img src="images/demo-logo.png" alt="" srcset=""></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse"
+          data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+          aria-label="Toggle navigation">
+            <i class="fa fa-bars"></i>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="#top">Inicio </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#about-us">Nosotros</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#services">Servicios</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#testimonials">Testimonios</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link " href="#" >Blog</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#footer">Contacto</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+    </section>
+   
+   <section id="banner">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <p class="promo-title">Programando Ideas</p>
+            <p class="text-justify">Soporte Técnico, Sistema de ventas, sistema de notas y mucho mas</p>
+            <a href=""><img src="images/play.png" class="play-btn">Leer Mas</a>
+          </div>
+          <div class="col-md-6 text-center">
+            <img src="images/home2.png" alt="" class="img-fluid">
+          </div>
+        </div>
+      </div>
+      <img src="images/wave1.png" alt="" class="bottom-img">
+   </section>
+   
+   <!--services-->
+   <section id="services">
+    <div class="container text-center">
+    <h1 class="title">Portafolio de Servicios</h1>
+      <div class="row text-center">
+        <div class="col-md-4 services">
+          <img src="images/service1.png" alt="" class="services-img">
+          <h4>Diseño web</h4>
+          <p class="text-justify">Hoy en día la página web es un elemento indispensable para cualquier empresa ya que representa el primer punto de contacto con el cliente en Internet.
+          ,Nuestros diseñadores web combinan la funcionalidad, el diseño y el buen gusto a la hora de diseñar y desarrollar su web.</p>
+        </div>
+        
+        <div class="col-md-4 services">
+          <img src="images/service2.png" alt="" class="services-img">
+          <h4>Plataformas Tecnológicas</h4>
+          <p>ofrecemos webs 100% a tu medida. Contamos con un equipo integral de creativos, diseñadores y programadores para llevar adelante el proyecto y que el cliente se pueda sentir libre de diseñar la página que siempre quiso..</p>
+        </div>
+        <div class="col-md-4 services">
+          <img src="images/service3.png" alt="" class="services-img">
+          <h4>Soporte Tecnico</h4>
+          <p class="text-justify">Mantenimiento de computadores: 
+          Es un servicio en el cual administramos el área de informática de su
+          empresa, realizamos mantenimientos preventivos programados
+          y soporte técnico virtual o presencial a sus computadores 
+          por un valor fijo mensual. 
+          Contamos con un avanzado Software Helpdesk vía Web
+          que le permite en todo momento conocer el estado de sus equipos .</p>
+        </div>
+      </div>
+      <button type="button" class="btn btn-primary">Leer Mas</button>
+    </div>
+    
+   </section>   
+
+<!-- about us -->
+  <section id="about-us">
+    <div class="container">
+      <h1 class="title text-center">   QUE HACEMOS   </h1>
+      <div class="row">
+        <div class="col-md-6">
+          <p class="about-title">Por qué somos diferentes</p>
+          <ul>
+            <li>Porque somos mucho más que una consultoría, prestamos un servicio integral a la empresa, 
+            para que el cliente no tenga que preocuparse de nada.</li>
+            <li>Porque somos un equipo de profesionales que trabajamos volcados para asegurar el éxito de nuestros clientes</li>
+            <li>Porque somos profesionales y trabajamos para asegurar el éxito de nuestros clientes</li>
+            <li>Porque realizamos proyectos personalizados a cada cliente</li>
+            <li>Porque aseguramos el cumplimiento de los plazos establecidos</li>
+          
+          </ul>
+        </div>
+        <div class="col-md-6">
+          <img src="images/network.png" alt="" class="img-fluid">
+        </div>
+      </div>
+    </div>
+  </section>
+  
+  <!--testimonios-->
+  <section id="testimonials">
+    <div class="container">
+    <h1 class="title text-center">Testimonios</h1>
+      <div class="row offset-1">
+        <div class="col-md-5 testimonials">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique nostrum quo dolorum non modi aliquid eius excepturi quaerat asperiores eos dolore quas, libero accusantium maxime ipsum, amet adipisci voluptatibus beatae?</p>
+          <img src="images/user3.jpg" alt="" srcset="">
+          <p class="user-details"><b>Jerson Batista </b> <br>Programador JKSYSTEMAS</p>
+        </div>
+        
+        <div class="col-md-5 testimonials">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique nostrum quo dolorum non modi aliquid eius excepturi quaerat asperiores eos dolore quas, libero accusantium maxime ipsum, amet adipisci voluptatibus beatae?</p>
+          <img src="images/user2.jpg" alt="" srcset="">
+          <p class="user-details"><b>Bill Gates </b> <br>Co-Fundador JKSYSTEMAS</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  
+  <!-- social media-->
+  
+  <section id="social-media">
+    <div class="container text-center">
+        <p>Nuestras Redes Sociales</p>
+        <div class="social-icons">
+          <a href=""><img src="images/facebook-icon.png"></a>
+          <a href=""><img src="images/instagram-icon.png"></a>
+          <a href=""><img src="images/twitter-icon.png"></a>
+          <a href=""><img src="images/whatsapp-icon.png"></a>
+          <a href=""><img src="images/linkedin-icon.png"></a>
+          <a href=""><img src="images/snapchat-icon.png"></a>
+        </div>
+    </div>
+  </section>
+  
+  
+  <section id="footer">
+    <img src="images/wave2.png" alt="" class="footer-img">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 footer-box">
+          <img src="images/demo-logo.png" alt="">
+          <p>Lorem ipsum dolor sit amet consectetur, 
+          adipisicing elit. Vel maxime nihil nisi exercitationem modi
+          perferendis deleniti, ducimus odio, ea molestias commodi officia 
+          tenetur magni. Corporis accusantium est incidunt ipsum id? </p>
+        </div>
+        
+        <div class="col-md-4 footer-box">
+         <p><b>CONTACTO</b> </p>
+         <p><i class="fa fa-map-marker"></i> OFICINAS:EL CARMEN DE BOLIVAR</p>
+         <p> <i class="fa fa-phone"></i> ingjerson2014@gmail.com</p>
+         <p><i class="fa fa-envelope"></i> cel:3007358024</p>
+        </div>
+        
+        <div class="col-md-4 footer-box">
+         <p>SUSCRIBETE</p>
+         <input type="email" name="" id="" class="form-control" placeholder="Email"><br>
+        <button type="button" class="btn btn-primary">Suscribete</button>
+        </div>
+      </div>
+      <hr>
+      <p class="derechos">WEB DEVELOPER: JERSON BATISTA 2021</p>
+    </div>
+  </section>
+    
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/smooth-scroll.js"></script>
+    <script>
+  	   var scroll = new SmoothScroll('a[href*="#"]');
+  </script>
+</body>
+</html>
